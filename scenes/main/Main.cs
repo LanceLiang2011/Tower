1833c4eaf3d6f578ea772eef049e73a67b0e3d7c
LanceLiang2011 <liangxianyi2011@gmail.com>
2025-07-25 22:59:26 -0400

    feat: reference GridManager and GoldMine nodes in Main class

--- scenes/main/Main.cs
+++ scenes/main/Main.cs
@@ -4,4 +4,19 @@ using Game.Resources.Building;
 
 namespace Game;
 
-public partial class Main : Node { }
+public partial class Main : Node
+{
+  private GridManager gridManager;
+  private Node2D goldMine;
+
+  public override void _Ready()
+  {
+    GetNodes();
+  }
+
+  private void GetNodes()
+  {
+    gridManager = GetNode<GridManager>("%GridManager");
+    goldMine = GetNode<Node2D>("%GoldMine");
+  }
+}
